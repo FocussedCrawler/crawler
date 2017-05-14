9a311f4691c79317341585ea393cd9ffad529c5a
mukul1862 <mukul.sharma1862@gmail.com>
2017-05-14 20:17:27 +0530

    Update trie.py

--- project/trie.py
+++ project/trie.py
@@ -1,5 +1,12 @@
 import sys
+import time
 from collections import deque
+import csv
+import pandas as pd
+import xlrd
+from collections import defaultdict
+from Lib import linecache
+from e import *
 
 
 class Node:
@@ -84,13 +91,83 @@ if __name__ == '__main__':
 
     file = open("read.txt", "r")
     count=0
+    flag=0
+    weight=0
 
+    
     for line in file:
         count=count+1
         t.insert(line)
-    
-    person=input()
-    a=t.get_possible_words(person)
-    b=t.__contains__("mukul")
-    print(a)
-    print(b)
+
+    file.close()
+    print(" enter the seed url")
+    url_initial=input()
+
+    file=open("IMPORTANTLINKS.txt","w")
+    file.write(url_initial + '\n')
+    file.close()
+
+    a=-1
+    while(1) :
+        #
+        a=a+1
+        start=time.time()
+        #s=linecache(read the a line)
+        s=linecache.getline("IMPORTANTLINKS.txt",(a+1))
+        scrap(s,a)
+        linecache.clearcache()
+        path = 't.xlsx'
+        workbook = xlrd.open_workbook(path)
+        ring=str(a)
+        #f=open('currenturl.txt','r')
+        #sheet_name=f.read()
+        #f.close()
+        worksheet= workbook.sheet_by_name(ring)
+        i=worksheet.nrows
+        j=worksheet.ncols
+        i=i-1
+        j=j-1
+        for I in range(0,i):
+            flag=0
+            count=0
+            weight=0
+            for J in range(0,j):
+                word=worksheet.cell_value(I,J)
+                word=word.replace(',',' ')
+                count=0
+                if(J==0 or J==2):
+                    for m in word.split(" "):
+                        b=t.__contains__(m)
+                        if b==True:
+                            count=count+1;
+
+                    weight=weight+(count*20)
+
+
+
+                if(J==1):
+                    link_associated=worksheet.cell_value(I,J)
+                    continue
+
+                if(J==3 or J==4):
+                    for m in word.split(" "):
+
+
+                        b=t.__contains__(m)
+                        if b==True:
+
+                            count=count+1;
+
+                    weight=weight+(count*3)
+
+            if(weight>10):
+                print(link_associated)
+                file = open("IMPORTANTLINKS.txt", "a+")
+                file.write(link_associated)
+                file.write("\n")
+                file.close()
+            
+
+
+        print(time.time()-start)
+        #input()
